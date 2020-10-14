bd4174ef9268d4ae393bbc67ad3fddf5a571bc12
5hkj <hyun-kyung@outlook.com>
2020-10-13 18:17:05 -0700

    upload completed app

---
+++ app/src/main/java/com/hyunkyung/cs4750/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package com.hyunkyung.cs4750.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
